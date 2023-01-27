6b8ad5c58526ecc16d3c4e515fe551b8980630bf
Setu Gupta <setu.gupta.2020@gmail.com>
2023-01-27 18:09:04 +0530

    Added all the code and changed the file structure
    The code segfaults as of this commit. The reason for this is not known.
    
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	deleted:    README
    	deleted:    debug/.gitignore
    	renamed:    tests/Makefile -> examples/Makefile
    	new file:   examples/README
    	renamed:    tests/fib.cpp -> examples/fib.cpp
    	renamed:    tests/matrix.cpp -> examples/matrix.cpp
    	renamed:    tests/vector.cpp -> examples/vector.cpp
    	renamed:    Makefile -> library/Makefile
    	new file:   library/libstamp.so
    	new file:   library/stamp.cpp
    	renamed:    src/stamp.h -> library/stamp.h
    	deleted:    release/.gitignore
    	deleted:    src/stamp.cpp
    	deleted:    tests/debug/.gitignore
    	deleted:    tests/release/.gitignore

---
+++ library/stamp.cpp
@@ -0,0 +1,149 @@
+#include <stamp.h>
+
+template<typename T>
+void create_thread(pthread_t &thread, T lambda)
+{
+        auto wrapper = [](void* data) -> void* {
+                return (*static_cast<decltype(lambda)*>(data))();
+        };
+
+        int error = pthread_create(&thread, NULL, wrapper, &lambda);
+        if(error)
+        {
+                std::cerr << "Failed to create thread: " << std::strerror(error);
+                std::exit(-1);
+        }
+}
+
+void create_thread_no_arg(pthread_t &thread, std::function<void()> &&lambda)
+{
+        auto _lambda = [&]() -> void* {
+                lambda();
+                return nullptr;
+        };
+
+        create_thread(thread, _lambda);
+}
+
+void create_thread_int(pthread_t &thread, std::function<void(int)> &&lambda, int arg)
+{
+        auto _lambda = [&]() -> void* {
+                lambda(arg);
+                return nullptr;
+        };
+
+        create_thread(thread, _lambda);
+}
+
+void create_thread_int_int(pthread_t &thread, std::function<void(int, int)> &&lambda, int arg1, int arg2)
+{
+        auto _lambda = [&]() -> void* {
+                lambda(arg1, arg2);
+                return nullptr;
+        };
+
+        create_thread(thread, _lambda);
+}
+
+void join_thread(pthread_t &thread)
+{
+        int error = pthread_join(thread, NULL);
+        if(error)
+        {
+                std::cerr << "Failed to join thread: " << std::strerror(error);
+                std::exit(-1);
+        }
+}
+
+// accepts two C++11 lambda functions and runs the them in parallel
+void stamp::execute_tuple(std::function<void()> &&lambda1, std::function<void()> &&lambda2)
+{
+        pthread_t thread1, thread2;
+        create_thread_no_arg(thread1, std::move(lambda1));
+        create_thread_no_arg(thread2, std::move(lambda2));
+
+        join_thread(thread1);
+        join_thread(thread2);
+}
+
+// parallel_for accepts a C++11 lambda function and runs the loop body (lambda) in 
+// parallel by using ‘numThreads’ number of Pthreads created inside StaMp
+void stamp::parallel_for(int low, int high, int stride, std::function<void(int)> &&lambda, int numThreads)
+{
+        int count = (high - low)/stride;
+        pthread_t* threads = (pthread_t*)malloc(count * sizeof(pthread_t));
+
+        int running_threads = 0;
+        int next_thread_to_join = 0;
+        int arg = low;
+        for(int i = 0; i < count; i += 1)
+        {
+                create_thread_int(threads[i], std::move(lambda), arg);
+                arg += stride;
+                running_threads++;
+
+                // Join older threads if running threads is greater than or equal to maximum allowed threads
+                if(running_threads >= numThreads)
+                {
+                        join_thread(threads[next_thread_to_join++]);
+                        running_threads--;
+                }
+        }
+        
+        for(int i = next_thread_to_join; i < count; i += 1)
+                join_thread(threads[i]);
+        
+        free(threads);
+}
+
+// Shorthand for using parallel_for when lowbound is zero and stride is one.
+void stamp::parallel_for(int high, std::function<void(int)> &&lambda, int numThreads)
+{
+        stamp::parallel_for(0, high, 1, std::move(lambda), numThreads);
+}
+
+// This version of parallel_for is for parallelizing two-dimensional for-loops, i.e., an outter for-i loop and 
+// an inner for-j loop. Loop properties, i.e. low, high, and stride are mentioned below for both outter 
+// and inner for-loops. The suffixes “1” and “2” represents outter and inner loop properties respectively. 
+void stamp::parallel_for(int low1, int high1, int stride1, int low2, int high2, int stride2, std::function<void(int, int)> &&lambda, int numThreads)
+{
+        int outer_count = (high1 - low1)/stride1;
+        int inner_count = (high2 - low2)/stride2;
+
+        pthread_t* threads = (pthread_t*)malloc(outer_count * inner_count * sizeof(pthread_t));
+        
+        int running_threads = 0;
+        int next_thread_to_join = 0;
+        int outer_arg = low1;
+        for(int i = 0; i < outer_count; i += 1)
+        {
+                int inner_arg = low2;
+                for(int  j = 0; j < inner_count; j++)
+                {
+                        create_thread_int_int(threads[(i * inner_count) + j], std::move(lambda), outer_arg, inner_arg);
+                        inner_arg += stride2;
+                        running_threads++;
+                
+                        // Join older threads if running threads is greater than or equal to maximum allowed threads
+                        if(running_threads >= numThreads)
+                        {
+                                join_thread(threads[next_thread_to_join++]);
+                                running_threads--;
+                        }
+                }
+                outer_arg += stride1;
+        }
+        
+        for(int i = next_thread_to_join; i < outer_count * inner_count; i += 1)
+                join_thread(threads[i]);
+        
+        free(threads);
+}
+
+// Shorthand for using parallel_for if for both outer and inner for-loops, lowbound is zero and stride is one.
+// In that case only highBounds for both these loop should be sufficient to provide. The suffixes “1” and “2”
+// represents outter and inner loop properties respectively.
+void stamp::parallel_for(int high1, int high2, std::function<void(int, int)> &&lambda, int numThreads)
+{
+        stamp::parallel_for(0, high1, 1, 0, high2, 1, std::move(lambda), numThreads);
+}
